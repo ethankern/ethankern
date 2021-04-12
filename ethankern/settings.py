1aef408eda817a3c650264021bc7ee492f55a02b
Ethan Kern <theethankern@gmail.com>
2021-04-12 15:37:34 -0700

    oops

--- ethankern/settings.py
+++ ethankern/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .local_settings import SECRET_KEY
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
